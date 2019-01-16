d86fa0bcbd6d170123b69e3bbb77cadee494dfeb
FSWL <fswl98@gmail.com>
2019-01-17 03:50:36 +0700

    added Generated Class and templates

---
+++ src/GeneratedClass.java
@@ -0,0 +1,22 @@
+public class GeneratedClass {
+    public static final GeneratedClass INSTANCE = new GeneratedClass();
+
+    private String name;
+    private boolean hasMain;
+
+    public String getName () {
+        return name;
+    }
+
+    public boolean isHasMain () {
+        return hasMain;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setHasMain (boolean hasMain) {
+        this.hasMain = hasMain;
+    }
+}
