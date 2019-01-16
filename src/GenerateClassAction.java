763def17aabe8e68202c741952fe4118edda9400
FSWL <fswl98@gmail.com>
2019-01-17 06:05:26 +0700

    added action class and changed plugin.xml

---
+++ src/GenerateClassAction.java
@@ -0,0 +1,36 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.psi.*;
+
+public class GenerateClassAction extends AnAction {
+    private PsiDirectory selectedDir;
+
+    @Override
+    public void update (AnActionEvent e) {
+        PsiElement selectedElement = CommonDataKeys.PSI_ELEMENT.getData(e.getDataContext());
+        if (selectedElement instanceof PsiDirectory)
+            selectedDir = (PsiDirectory) selectedElement;
+        else if (selectedElement instanceof PsiClass) {
+            PsiFile psiFile = selectedElement.getContainingFile();
+            selectedDir = psiFile.getContainingDirectory();
+        }
+        else
+            e.getPresentation().setEnabledAndVisible(false);
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+        GenerateDialogWrapper dialogWrapper = new GenerateDialogWrapper(anActionEvent.getProject());
+        dialogWrapper.show();
+        String temp = null;
+        if (dialogWrapper.isOK()) {
+
+            if (GeneratedClass.INSTANCE.isHasMain())
+                temp = "ClassWithMain.java";
+            else
+                temp = "BasicClass.java";
+        }
+        JavaDirectoryService.getInstance().createClass(selectedDir, GeneratedClass.INSTANCE.getName(), temp, true);
+    }
+}
